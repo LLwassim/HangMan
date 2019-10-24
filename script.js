4f53d229f2400fb58ba855703a81be60f117f501
Wassim Lkorchy <lacorchy77@gmail.com>
2019-10-24 09:33:40 -0400

    Animations Created

--- script.js
+++ script.js
@@ -142,7 +142,185 @@ function challengeGuess(){
     }
 }
 
+function countChars(countfrom,displayto) {
+    var len = document.getElementById(countfrom).value.length;
+    document.getElementById(displayto).innerHTML = len;
+}
 
+function readText(){
+    word = document.getElementById('input').value;
+    hangman();
+}
+
+function hangman(){
+    var x = word.length;
+        if(x==0){
+            alert("Please enter something into the text box.");
+            return;
+        }
+    var y = x-1;
+    var spaces = 0;
+    var validChar = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "?", "!", ",", ".", "-", "'");
+    for(z = 0; z < word.length; z++){
+        var letter = word.substring(y,x);
+        if(validChar.indexOf(letter) > -1){
+            x--;
+            y--;
+        }
+        else{
+            alert("Please remove any special characters.");
+            return;
+        }
+    }
+    x = word.length;
+    y = x-1;
+    while (x>0){
+        numChar++;
+        var letter = word.substring(y,x);
+        if(letter === " "){
+            document.getElementById('letter'+x).innerHTML = "&nbsp;";
+            document.getElementById('letter'+x).style.visibility = "hidden";
+            document.getElementById('letter'+x).style.display = "block";
+            document.getElementById('underline'+x).style.display = "block";
+            spaces++;
+        }
+        else if(letter === "?" || letter === "!" || letter === "," || letter === "." || letter === "-" || letter === "'"){
+            document.getElementById('letter'+x).innerHTML = letter;
+            document.getElementById('letter'+x).style.display = "block";
+            document.getElementById('underline'+x).style.display = "block";
+            spaces++;
+        }
+        else{
+            document.getElementById('letter'+x).innerHTML = letter;
+            document.getElementById('letter'+x).style.visibility = "hidden";
+            document.getElementById('underline'+x).style.display = "block";            
+            document.getElementById('underline'+x).style.borderBottom = "3px solid black";
+        }
+        x--;
+        y--;
+    }
+    phraseLength = word.length - spaces;
+    document.getElementById('multiPage').style.display = "none";
+    document.getElementById('gamePage').style.display = "block";
+    splitWords();
+    document.getElementById('challengeBank').style.display = "none";
+    draw();
+}
+
+function draw(){
+    var ctx = document.getElementById("hangman").getContext('2d');
+        ctx.fillStyle = "white";
+        ctx.lineWidth=3;
+        ctx.fillRect(0, 0, 300, 300);
+        ctx.beginPath(); //vertical bar
+            ctx.moveTo(50,270);
+            ctx.lineTo(50,25);
+            ctx.stroke();
+        ctx.beginPath(); //vertical bar long piece
+            ctx.moveTo(65,270);
+            ctx.lineTo(65,85);
+            ctx.stroke();
+        ctx.beginPath(); //vertical bar short piece
+            ctx.moveTo(65,64);
+            ctx.lineTo(65,40);
+            ctx.stroke();
+        ctx.beginPath(); //horizontal bar
+            ctx.moveTo(49,25);
+            ctx.lineTo(175,25);
+            ctx.stroke();
+        ctx.beginPath(); //horizontal bar short piece
+            ctx.moveTo(49,40);
+            ctx.lineTo(86,40);
+            ctx.stroke();
+        ctx.beginPath(); //horizontal bar long piece
+            ctx.moveTo(106,40);
+            ctx.lineTo(175,40);
+            ctx.stroke();
+        ctx.beginPath(); //small vertical bar
+            ctx.moveTo(173,25);
+            ctx.lineTo(173,40);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(50,80);
+            ctx.lineTo(100,25);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(60,90);
+            ctx.lineTo(111,35);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(50,80);
+            ctx.lineTo(60,90);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(100,25);
+            ctx.lineTo(111,35);
+            ctx.stroke();
+        ctx.beginPath(); //ground
+            ctx.moveTo(35,270);
+            ctx.lineTo(265,270);
+            ctx.stroke();
+        ctx.beginPath(); //noose
+            ctx.moveTo(150,40);
+            ctx.lineTo(150,80);
+            ctx.stroke();
+    var cntx = document.getElementById("homeHangman").getContext('2d');
+        cntx.fillStyle = "white";
+        cntx.lineWidth=3;
+        cntx.fillRect(0, 0, 300, 300);
+        cntx.beginPath(); //vertical bar
+            cntx.moveTo(50,270);
+            cntx.lineTo(50,25);
+            cntx.stroke();
+        cntx.beginPath(); //vertical bar long piece
+            cntx.moveTo(65,270);
+            cntx.lineTo(65,85);
+            cntx.stroke();
+        cntx.beginPath(); //vertical bar short piece
+            cntx.moveTo(65,64);
+            cntx.lineTo(65,40);
+            cntx.stroke();
+        cntx.beginPath(); //horizontal bar
+            cntx.moveTo(49,25);
+            cntx.lineTo(175,25);
+            cntx.stroke();
+        cntx.beginPath(); //horizontal bar short piece
+            cntx.moveTo(49,40);
+            cntx.lineTo(86,40);
+            cntx.stroke();
+        cntx.beginPath(); //horizontal bar long piece
+            cntx.moveTo(106,40);
+            cntx.lineTo(175,40);
+            cntx.stroke();
+        cntx.beginPath(); //small vertical bar
+            cntx.moveTo(173,25);
+            cntx.lineTo(173,40);
+            cntx.stroke();
+        cntx.beginPath(); //cross bar
+            cntx.moveTo(50,80);
+            cntx.lineTo(100,25);
+            cntx.stroke();
+        cntx.beginPath(); //cross bar
+            cntx.moveTo(60,90);
+            cntx.lineTo(111,35);
+            cntx.stroke();
+        cntx.beginPath(); //cross bar
+            cntx.moveTo(50,80);
+            cntx.lineTo(60,90);
+            cntx.stroke();
+        cntx.beginPath(); //cross bar
+            cntx.moveTo(100,25);
+            cntx.lineTo(111,35);
+            cntx.stroke();
+        cntx.beginPath(); //ground
+            cntx.moveTo(35,270);
+            cntx.lineTo(265,270);
+            cntx.stroke();
+        cntx.beginPath(); //noose
+            cntx.moveTo(150,40);
+            cntx.lineTo(150,80);
+            cntx.stroke();
+}
 
 
 
